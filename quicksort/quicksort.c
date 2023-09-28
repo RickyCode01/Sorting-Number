57ba313ced27040b4d409038f5b355aeaa91b6a1
Riccardo <riccardomanoni7@gmail.com>
2023-09-29 00:41:10 +0200

    update quicksort

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,58 +1,57 @@
+/* Algoritmo quicksort = è un tipo di algoritmo divide et impera che ha una complessità media
+computazionale pari a O(nlog n) ma non è un algoritmo ottimo come ad esempio il merge sort.
+A differenza di questo ultimo l'ordinamento viene eseguito nella procedura partition che si occupa
+di ordinare gli elementi dell'array a destra e sinistra di un elemento perno il quale si troverà
+nella sua posizione finale terminata la procedura. Richiamando questa in modo ricorsivo a dx e sx del perno
+viene eseguito l'odinamento */
+
 #include <stdlib.h>
 #include <stdio.h>
+#include "../ArrayLib/randArr.h"
 
-void swap(int* a, int* b){
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+void swap(int *A, int a, int b){
+    int temp = *(A+a);
+    *(A+a) = *(A+b);
+    *(A+b) = temp; 
 }
 
-void printarr(int arr[], int inizio, int fine){
-  for(int i = inizio; i <= fine; i++ ){
-    printf(" %d ", arr[i]);
+int Partition(int *A, int start, int end){
+  int pivot = *(A+start); // scelgo il pivot come primo valore dell'array
+  int i = start;
+  int j = end;
+  while(i < j){ // cicla fino ad aver controllato tutti gli elementi dell'array
+    while (A[j] > pivot)j--; // decremento a dx finchè trovo un valore > pivot
+    if(i < j){ // se non ho superato il pivot
+      printf("i:%d -> j:%d\n", i, j);
+      swap(A, i, j); // scambio gli elementi: pivot -> elemento dx > del pivot
+      printArray(A, start, end+1, false);
+      i++; // incremento l'indice sx perchè questo elemento è ordinato rispetto al pivot
+    }
+    /* di seguito ripeto la procedura appena applicata partendo dall'indice a sx dopo 
+    l'elemento appena scambiato */
+    while(A[i] < pivot)i++;
+    if(i < j){
+      printf("i:%d -> j:%d\n", i, j);
+      swap(A, i, j);
+      printArray(A, start, end+1, false);
+      j--;
+    }
   }
-  printf("\n");
-}
-
-int partition(int arr[], int start, int end){
-    //printarr(arr, start, end);
-    int pivot = arr[start];
-    printf("pivot:%d\n",pivot);
-    int pos = start; //variabile che memorizza posizione pivot
-    int i = start;
-    int j = end;
-    do{
-      if(arr[i] < pivot){
-        i++;
-      }else{
-        if(arr[j] < pivot){
-          printf(" %d -> %d\n", arr[i], arr[j]);
-          swap(&arr[i], &arr[j]);
-          if(arr[j] == pivot)pos = j; //salva la posizione del pivot dopo lo scambio
-          i++;
-        }else{j--;}
-      }
-    }while(i<j);
-    swap(&arr[pos], &arr[j]); //metto il pivot nella giusta posizione nell'arr
-    return j;
+  return i; // ritorno l'ultima posizione del pivot
 }
 
 void Quicksort(int arr[], int start, int end){
   if (start < end) {
-      int pindex = partition(arr, start, end); //indice di partizione
-      //printf("pi:%d\n", pindex);
-      Quicksort(arr, start, pindex - 1); //partizione bassa
-      Quicksort(arr, pindex + 1, end); // partizione alta
-    }
+    int pindex = Partition(arr, start, end); //indice di partizione
+    //printf("pi:%d\n", pindex);
+    Quicksort(arr, start, pindex - 1); //partizione bassa
+    Quicksort(arr, pindex + 1, end); // partizione alta
+  }
 }
 
 int main(){
-
-    int a = 0;
-    int b = 0;
-    int size = 9;
-    int arr[] = {1,5,7,-9,3,-3,15,0,4,10};
-    printarr(arr, 0, size);
-    Quicksort(arr, 0, size);
-    printarr(arr, 0, size);
+  int *A = randIntArr(10, 0, 10);
+  printArray(A, 0, 10, false);
+  Quicksort(A, 0, 9);
+  printArray(A, 0, 10, false);
 }
